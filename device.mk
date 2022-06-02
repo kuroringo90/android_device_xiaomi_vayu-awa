99118ae9190070fbb3e05b5df125d0e1848baac8
CrisBal <74150843+CBG-leeco@users.noreply.github.com>
2022-06-02 02:42:07 +0200

    vayu: Build mock hidl power stats
    * Fix for hidl power stats errors.
       BatteryStatsService: Unable to load Power Hal or power.stats HAL
       hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    
    Change-Id: If691078015f00db31b6613bafbd5ca4dc3246d5c

--- device.mk
+++ device.mk
@@ -315,7 +315,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-     android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 # QTI
 PRODUCT_PACKAGES += \
