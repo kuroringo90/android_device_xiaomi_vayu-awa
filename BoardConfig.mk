671c641b32e6013588e3f68dfdb2fc4aa5b0aa24
kuroringo90 <ssss9999@gmx.it>
2022-06-02 02:07:43 +0200

    vayu: Declare BOARD_VENDOR
    * Needed for xiaomi-perfmgr power AIDL guards

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,6 +168,7 @@ BOARD_USES_METADATA_PARTITION := true
 
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
+BOARD_VENDOR := xiaomi
 
 # Power
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
