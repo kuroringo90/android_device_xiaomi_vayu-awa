68ecb81785c6352e8bfded4a6dde1cddfe5cb1b4
kuroringo90 <ssss9999@gmx.it>
2022-06-04 00:38:23 +0200

    Power: import power hal (attempt to fix dt2w)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,7 +170,10 @@ BOARD_USES_METADATA_PARTITION := true
 TARGET_BOARD_PLATFORM := msmnile
 
 # Power
-
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+TARGET_USES_INTERACTION_BOOST := true
+TARGET_POWERHAL_BOOST_EXT := $(DEVICE_PATH)/power/boost-ext.cpp
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/mode-ext.cpp
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
