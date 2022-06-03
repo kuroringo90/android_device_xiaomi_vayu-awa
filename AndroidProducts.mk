683be7a6006432defdb89ebff348e62cfe35d7c4
kuroringo90 <ssss9999@gmx.it>
2022-06-04 01:21:17 +0200

    vayu: welcome to project zephyrus

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_vayu.mk
+    $(LOCAL_DIR)/vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_vayu-user \
-    superior_vayu-userdebug \
-    superior_vayu-eng
+    vayu-user \
+    vayu-userdebug \
+    vayu-eng
