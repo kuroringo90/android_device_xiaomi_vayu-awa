73f965c4904eb0820d53a5bdcf6cc5bfa9cf0b53
kuroringo90 <ssss9999@gmx.it>
2022-06-11 16:47:21 +0200

    Revert "vayu: Overlay RefreshRate strings"
    This reverts commit 9bd085aeba2678d8bf539268b3064e0c1c61aa4f.

--- device.mk
+++ device.mk
@@ -303,8 +303,6 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-PRODUCT_PACKAGES += \ SettingsOverlayRefreshRate
-
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
 # Partitions
