7e4f707ac7d0a4e01ff2d23427bbaa5937c52cb4
Pranav Vashi <neobuddy89@gmail.com>
2022-06-16 07:24:22 +0200

    vayu: Update build fingerprint from V13.0.4.0.SJUMIXM

--- vayu.mk
+++ vayu.mk
@@ -33,9 +33,6 @@ $(call inherit-product, vendor/lawnchair/lawnchair.mk)
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vayu_global-user 12 SKQ1.211006.001 V13.0.3.0.SJUMIXM release-keys" \
-    PRODUCT_NAME=vayu_global \
-    PRODUCT_MODEL=M2102J20SI
+    PRIVATE_BUILD_DESC="vayu_global-user 12 RKQ1.200826.002 V13.0.4.0.SJUMIXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.3.0.SJUMIXM:user/release-keys
-
+BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.4.0.SJUMIXM:user/release-keys
