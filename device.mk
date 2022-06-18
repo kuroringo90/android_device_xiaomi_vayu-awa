075f61e6a7c00643070eec576c4e108cf2132e20
kuroringo90 <ssss9999@gmx.it>
2022-06-18 06:45:33 +0200

    vayu: setup nfc stock

--- device.mk
+++ device.mk
@@ -288,7 +288,8 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service
+    android.hardware.nfc@1.2-service \
+    android.hardware.nfc@1.2.vendor 
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
