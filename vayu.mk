79c119653fcbbcd8c8061e3568ff8cde121596b0
kuroringo90 <ssss9999@gmx.it>
2022-06-26 11:49:00 +0200

    vayu: welcome to project zephyrus

--- vayu.mk
+++ vayu.mk
@@ -11,32 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit some common Awaken stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
-
 # Boot Animation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# GApps
-TARGET_GAPPS_ARCH := arm64
-USE_GAPPS := true
-
-# Google Recorder
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-
-# Google Assistant
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-
-# Official
-AWAKEN_BUILD_TYPE := OFFICIAL
-
-# Quick Tap
-TARGET_SUPPORTS_QUICK_TAP := true
-
-PRODUCT_NAME := awaken_vayu
+PRODUCT_NAME := vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
+
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGER_IMAGES := true
+TARGET_SUPPORTS_QUICK_TAP := true
+WITH_GMS := true
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
